934de919bd89800b7c6a9c9109dad51eeefa46a3
JuanIgnacioMarchetto <43477348+JuanIgnacioMarchetto@users.noreply.github.com>
2023-01-25 17:59:16 -0300

    rainbowfriends

---
+++ mail.js
@@ -0,0 +1,26 @@
+
+function Blue() {
+    document.write("<div  ><h1 >PERDISTE</h1><img src='./blue.jpeg' alt='' ></div>");
+}
+function Red() {
+    document.write("<div><h1 >PERDISTE</h1><img src='./red.jpeg' alt=''></div>");
+}
+function Orange() {
+    document.write("<div><h1 >PERDISTE</h1><img src='./oranje.jpeg' alt=''></div>");
+}
+function Green() {
+    document.write("<div><h1 >PERDISTE</h1><img src='./green.jpeg' alt=''></div>");
+}
+function Purple() {
+    document.write("<div><h1 >PERDISTE</h1><img src='./purple.jpeg' alt=''></div>");
+}
+function Yellow() {
+    document.write("<div><h1 >PERDISTE</h1><img src='./yellow.jpeg' alt=''></div>");
+}
+function Pink() {
+    document.write("<div><h1 >PERDISTE</h1><img src='./pink.jpeg' alt=''></div>");
+}
+
+function Ganaste() {
+       document.write("<div><h1 >GANASTE</h1><img src='./trofeo.png' alt=''> </div>")
+}
